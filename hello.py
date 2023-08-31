632ad3d537852bb5ceefbe55373936b5d370368b
cflores90 <cflores90@charlottesvilleschools.org>
2023-08-31 15:55:16 -0400

    servo wires

--- hello.py
+++ hello.py
@@ -1,5 +1,29 @@
-from time import sleep
+    
+#
+# SPDX-License-Identifier: MIT
+
+
+
+import time
+import board
+import touchio
+import pwmio
+from adafruit_motor import servo
+
+# create a PWMOut object on Pin A2.
+pwm = pwmio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
+
+# Create a servo object, my_servo.
+my_servo = servo.ContinuousServo(pwm)
+
+touch_A4 = touchio.TouchIn(board.A4)  # Not a touch pin on Trinket M0!
+touch_A5 = touchio.TouchIn(board.A5)  # Not a touch pin on Trinket M0!
 
 while True:
-    print("Hello world!!!!!")
-    sleep(1)
+    my_servo.throttle = 0.0
+    while touch_A4.value:
+        my_servo.throttle = 1.0
+        time.sleep(.5)
+    while touch_A5.value:
+        my_servo.throttle = -1.0
+        time.sleep(.5)
